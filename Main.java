907debf873db6d9166040102a014cb6c1c9112d2
Nadir Fouka <nfouka@users.noreply.github.com>
2019-01-22 21:35:05 +0100

    GET RAM

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.lang.management.ManagementFactory;
+
+import javax.management.AttributeNotFoundException;
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanException;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import javax.management.ReflectionException;
+
+public class Main {
+  public static void main(String[] args) throws InstanceNotFoundException, AttributeNotFoundException, MalformedObjectNameException, ReflectionException, MBeanException  {
+  /* Total number of processors or cores available to the JVM */
+  
+
+      MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+      Object attribute = mBeanServer.getAttribute(new ObjectName("java.lang","type","OperatingSystem"), "TotalPhysicalMemorySize");
+      Object attribute2 = mBeanServer.getAttribute(new ObjectName("java.lang","type","OperatingSystem"), "FreePhysicalMemorySize");
+      System.out.println("Total memory: "+ Long.parseLong(attribute.toString()) / 1024  +"MB");
+      System.out.println("Free  memory: "+ Long.parseLong(attribute2.toString()) / 1024  +"MB");	
+
+ }
+}
